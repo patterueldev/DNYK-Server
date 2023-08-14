f914a0955dbc2d76966fee1510e43bfb56129979
John Patrick Teruel <jpteruel95@gmail.com>
2023-08-14 09:17:00 +0800

    Initialize sample api

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
-class Person {
-  private name: string;
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-  constructor(name: string) {
-    this.name = name;
-  }
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
-  public sayHello(): void {
-    console.log(`Hello, my name is ${this.name}. This is a sea urchin.`);
-  }
+function parseOpenAPISpec(file: string): any {
+  const yamlContent = fs.readFileSync(file, 'utf8');
+  const parsedData = yaml.load(yamlContent);
+  return parsedData;
 }
 
-const person = new Person("John Doe");
-person.sayHello();
+// Replace 'openapi.yaml' with the path to your OpenAPI YAML file
+const openApiSpec = parseOpenAPISpec('openapi.yaml');
+console.log(openApiSpec);
+
+const app = express();
+
+// Serve the Swagger UI at /api-docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiSpec));
+
+// Add other routes and middleware as needed
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
