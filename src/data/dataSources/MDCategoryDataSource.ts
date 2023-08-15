9cb97f8411f2b08b3b5db6ff5bcea6a0a4d6b856
John Patrick Teruel <jpteruel95@gmail.com>
2023-08-15 10:19:06 +0800

    feat: Additional test updates

--- src/data/dataSources/MDCategoryDataSource.ts
+++ src/data/dataSources/MDCategoryDataSource.ts
@@ -1,12 +1,24 @@
 import { ICategory } from "../../domain/entities/ICategory";
 import { ICategoryRepository } from "../repositories/ICategoryRepository";
 import { MongoClient, Collection } from "mongodb";
+import mongoose, { Document, Schema } from 'mongoose';
+
+
+const categorySchema = new Schema<ICategory>({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  groupId: { type: String, required: true },
+});
+
+export const MDCategory = mongoose.model<ICategory>('MDCategory', categorySchema);
+
 
 export class MDCategoryDataSource implements ICategoryRepository {
   private collection: Collection;
 
   constructor() {
     // Initialize MongoDB connection and collection
+    this.collection = new Collection();
     const uri = "mongodb+srv://patteruel-dev:<password>@cluster0.thspzcw.mongodb.net/?retryWrites=true&w=majority"; // Update with your MongoDB connection URI
     const dbName = "dnyk"; // Update with your database name
     const client = new MongoClient(uri);
@@ -20,27 +32,30 @@ export class MDCategoryDataSource implements ICategoryRepository {
   }
 
   async findById(id: string): Promise<ICategory | null> {
-    const category = await this.collection.findOne({ _id: id });
-    return category ? category as ICategory : null;
+    throw new Error("Method not implemented.");
+    // const category = await this.collection.findOne({ _id: id });
+    // return category ? category as ICategory : null;
   }
 
   async findAll(): Promise<ICategory[]> {
-    const categories = await this.collection.find().toArray();
-    return categories as ICategory[];
+    throw new Error("Method not implemented.");
+    // const categories = await this.collection.find().toArray();
+    // return categories as ICategory[];
   }
 
   async create(name: string, groupId: string): Promise<ICategory> {
-    const category: ICategory = { name, groupId };
-    const result = await this.collection.insertOne(category);
-    return result.ops[0] as ICategory;
+    const category = new MDCategory({ name, groupId });
+    await category.save();
+    return category;
   }
 
   async update(category: ICategory): Promise<void> {
-    await this.collection.updateOne({ _id: category.id }, { $set: category });
+    throw new Error("Method not implemented.");
+    // await this.collection.updateOne({ _id: category.id }, { $set: category });
   }
 
   async delete(id: string): Promise<void> {
-    await this.collection.deleteOne({ _id: id });
+    throw new Error("Method not implemented.");
+    // await this.collection.deleteOne({ _id: id });
   }
 }
-
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import swaggerUi from 'swagger-ui-express';
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 
+import router from './router.js';
+
 function parseOpenAPISpec(file: string): any {
   const yamlContent = fs.readFileSync(file, 'utf8');
   const parsedData = yaml.load(yamlContent);
@@ -18,6 +20,7 @@ const app = express();
 
 // Serve the Swagger UI at /api-docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiSpec));
+app.use('/api', router);
 
 // Add other routes and middleware as needed
 
